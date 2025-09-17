428eb70b6cfe034c03b1aa234511d05d74c6b447
ritesh <riteshpatidar088@gmail.com>
2025-09-17 11:27:41 +0530

    added form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,30 @@
-import { useState } from 'react';
-function App() {
-  console.log(useState('REACT'));
-  let [newTechnology, setNewTechnology] = useState('ANGULAR');
- const [name , setName] = useState('TEST')
- const [email,setEmail] = useState('test@gmail.com')
- const [password ,setPassword] = useState('2421@123') ;
+import { useState } from "react";
 
-  const handleClick = () => {
-    setNewTechnology('REACT');
-  };
-  const handleUpdateClick = () => {
-    setName('NEW-TEST')
-    setPassword('12345')
-    setEmail('newtest@gmail.com')
+function App(){
+  const [email ,setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) =>{
+console.log(event.target.value)
+setEmail(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+console.log(event.target.value)
   }
   return (
     <div>
-      <h1>{newTechnology}</h1>
-      <h1>{`${name} ${email} ${password}`}</h1>
-      <button onClick={handleUpdateClick}>Click to update</button>
-      <button onClick={handleClick}>Update</button>
+      <h1>FORM HANDLING---</h1>
+      <h1>GETTING VALUES OUT OF THE FORM INPUT FIELDS AND SUBMIT THE VALUES AS AN OBJECT</h1>
+      <form>
+        <label>Email</label>
+        <input onChange={handleEmailChange} type="email" name="email"  />
+        <label>Password</label>
+        <input onChange={handlePasswordChange} type="password" name="password" />
+      </form>
+      <p>Email : {email}</p>
     </div>
-  );
+  )
 }
 
 export default App;
-
-//hooks  => HOOKS ARE THE REACT FUNCTIONS
-//NOTE HOOKS CAN ONLY BE CALLED INSIDE THE FUNCTIONAL COMPONENT AT TOP LEVEL
-//NOTE hooks allow us to use react features like state.
-//NOTE State refers to that data which changes over the time.
-//NOTE TO SAVE this type of data we use state varible
-//NOTE TO manage state data we use react's useState() hook 
-//NOTE UseState accepts initial value as a argument 
-//NOTE useState struture 
-// const [stateVariableName , setStateVariableFnName] = useState(initialvalue of stateVariable)
-//NOTE The work of stateVariableName is to show data on the ui
-//NOTE this state varaible only going to update using the setter function.
-//NOTE we cannot directly update the state variable like this stateVariable = 'some value' ❌ always use the setter function to update the state variable
-
-//create three state variables like name , email , password ;
-// update all this at once when user click on a button
-
-//create a button when i click on the button changes its text .
-//create two button when i click on a button swap both btns title ;
