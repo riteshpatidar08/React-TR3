22acd47bcb9d58d85d08cf794b885b28dd1260f5
ritesh <riteshpatidar088@gmail.com>
2025-09-17 11:54:23 +0530

    udpated tilll the form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-function App(){
-  const [email ,setEmail] = useState('');
+function App() {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleEmailChange = (event) =>{
-console.log(event.target.value)
-setEmail(event.target.value)
-  }
+  const handleEmailChange = (event) => {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  };
 
   const handlePasswordChange = (event) => {
-console.log(event.target.value)
-  }
+    console.log(event.target.value);
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+    };
+    console.log(data);
+  };
   return (
     <div>
       <h1>FORM HANDLING---</h1>
-      <h1>GETTING VALUES OUT OF THE FORM INPUT FIELDS AND SUBMIT THE VALUES AS AN OBJECT</h1>
-      <form>
+      <h1>
+        GETTING VALUES OUT OF THE FORM INPUT FIELDS AND SUBMIT THE VALUES AS AN
+        OBJECT
+      </h1>
+      <form onSubmit={handleSubmit}>
         <label>Email</label>
-        <input onChange={handleEmailChange} type="email" name="email"  />
+        <input onChange={handleEmailChange} type="email" name="email" />
         <label>Password</label>
-        <input onChange={handlePasswordChange} type="password" name="password" />
+        <input
+          onChange={handlePasswordChange}
+          type="password"
+          name="password"
+        />
+        <button>Submit</button>
       </form>
       <p>Email : {email}</p>
+      <p>Password : {password}</p>
     </div>
-  )
+  );
 }
 
 export default App;
