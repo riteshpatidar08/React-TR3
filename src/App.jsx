5f41e19294eba98710dfdace39a678209522aa47
ritesh <riteshpatidar088@gmail.com>
2025-09-17 09:17:01 +0530

    udpated till the events

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-function App(){
-
-    // NOTE event handler function which triggers on the button or element click  where you have passed this fn
-    // Event name always start with handle 
-
-  const handleLoginClick = ()=>{
-    alert('login click')
-  }
-  //NOTE event handler for another event 
-  const handlePrintName = (name)=>{
-    alert(`hi ${name}`)
-  }
-  return <div>
-    //directly pass handler function in button
-
-<button onClick={()=>console.log('hi')}>Click</button>
-
-<button onClick={handleLoginClick}>Login</button>
-//if you have to pass arugment in handler
-<button onClick={()=>{handlePrintName('test')}} >Print Name</button>
-  </div>
+function App() {
+  // NOTE event handler function which triggers on the button or element click  where you have passed this fn..
+  //NOTE Event name always start with handle..
+  
+  const handleLoginClick = () => {
+    alert('login click');
+  };
+  //NOTE event handler for another event
+  const handlePrintName = (name) => {
+    alert(`hi ${name}`);
+  };
+  return (
+    <div>
+      //NOTE directly pass handler function in button
+      <button onClick={() => console.log('hi')}>Click</button>
+      <button onClick={handleLoginClick}>Login</button>
+      //NOTE if you have to pass argument in handler use a callback fn to call
+      the handler which is accepting arguments
+      <button
+        onClick={() => {
+          handlePrintName('test');
+        }}
+      >
+        Print Name
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
