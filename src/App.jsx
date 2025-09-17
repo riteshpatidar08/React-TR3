6fdf208d138cab0fb69038ea8080c00349d42133
ritesh <riteshpatidar088@gmail.com>
2025-09-17 09:15:12 +0530

    updated till the events

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ function App(){
   }
   return <div>
     //directly pass handler function in button
+
 <button onClick={()=>console.log('hi')}>Click</button>
+
 <button onClick={handleLoginClick}>Login</button>
 //if you have to pass arugment in handler
 <button onClick={()=>{handlePrintName('test')}} >Print Name</button>
