e9c143927cf0433e2a90b52192f64c77a3011ad4
ritesh <riteshpatidar088@gmail.com>
2025-09-24 11:39:49 +0530

    updated till the navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 function Navbar() {
+  const navlinks = [
+    { to: '/', title: 'Home' },
+    { to: '/services', title: 'Services' },
+    { to: '/about-us', title: 'About us' },
+  ];
   return (
     <header>
       <nav className="flex gap-6">
@@ -13,6 +18,12 @@ function Navbar() {
         <Link className="text-3xl font-bold" to="/about-us">
           About
         </Link>
+
+        {navlinks.map((nav) => (
+          <Link className="text-3xl font-bold" to={nav.to}>
+            {nav.title}
+          </Link>
+        ))}
       </nav>
     </header>
   );
