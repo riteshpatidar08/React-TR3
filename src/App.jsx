d011f9c084d9b7da4c5c4f5a1022916bb9d4fe4b
ritesh <riteshpatidar088@gmail.com>
2025-09-24 09:47:54 +0530

    updated till useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,40 @@
-import { useState } from 'react';
-
+import React from 'react';
+import { useEffect , useState} from 'react';
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name , setName] = useState(
+    'test'
+  )
+  const [data, setData] = useState([])
+  const [lastName, setLastName] = useState('singh')
+  useEffect(()=>{
+    console.log('run on everytime')
+  })
 
-  const handleEmailChange = (event) => {
-    console.log(event.target.value);
-    setEmail(event.target.value);
-  };
+  useEffect(()=>{
+    console.log('run on initial render')
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then((response) => response.json())
+    .then((json) => setData(json));
+  },[])
+ 
+  useEffect(()=>{
+    console.log('run on every time when name change')
+  },[name,lastName])
 
-  const handlePasswordChange = (event) => {
-    console.log(event.target.value);
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = {
-      email: email,
-      password: password,
-    };
-    console.log(data);
-  };
+    const handleClick  = () => {
+      setName('NEW-TEST')
+    }
+    const handleLastNameClick = () =>{
+      setLastName('Last-name')
+    }
   return (
     <div>
-      <h1>FORM HANDLING---</h1>
-      <h1>
-        GETTING VALUES OUT OF THE FORM INPUT FIELDS AND SUBMIT THE VALUES AS AN
-        OBJECT
-      </h1>
-      <form onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input onChange={handleEmailChange} type="email" name="email" />
-        <label>Password</label>
-        <input
-          onChange={handlePasswordChange}
-          type="password"
-          name="password"
-        />
-        <button>Submit</button>
-      </form>
-      <p>Email : {email}</p>
-      <p>Password : {password}</p>
+      <h1>Use effect</h1>
+      <h2>{name}</h2>
+      <h1>hllo</h1>
+      <h1>{lastName}</h1>
+      <button onClick={handleClick}>Click</button>
+      <button onClick={handleLastNameClick}>Change lastname</button>
     </div>
   );
 }
