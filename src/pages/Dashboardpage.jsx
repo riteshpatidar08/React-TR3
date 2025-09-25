d7f6136181ac5235f7a6ddf8539d462fcb4ef926
ritesh <riteshpatidar088@gmail.com>
2025-09-25 10:58:58 +0530

    udpated till nested routing

--- src/pages/Dashboardpage.jsx
+++ src/pages/Dashboardpage.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 function Dashboardpage() {
   return (
-    <div className='flex'>
-      <aside className='bg-blue-700 flex flex-col gap-2 text-white font-bold text-2xl justify-center px-10 w-96 h-screen'>
-        <Link to='/dashboard/overview'>Overview</Link>
-        <Link to='/dashboard/customers'>Customers</Link>
+    <div className="flex">
+      <aside className="bg-blue-700 flex flex-col gap-2 text-white font-bold text-2xl justify-center px-10 w-96 h-screen">
+        <Link to="/dashboard/overview">Overview</Link>
+        <Link to="/dashboard/customers">Customers</Link>
         <Link>Integration</Link>
       </aside>
-      <div className='ml-104'>
-      <Outlet/>
+      <div className="ml-104">
+        <Outlet />
       </div>
     </div>
-  )
+  );
 }
 
-export default Dashboardpage
+export default Dashboardpage;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,15 +10,15 @@ function Login() {
       navigate('/');
     }
   };
-  
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input type="text" />
-        <label>Password</label>
-        <input type="password" />
-        <button>Login</button>
+    <div className='h-screen flex  justify-center mt-30'>
+      <form onSubmit={handleSubmit} className='flex h-96  flex-col gap-5 bg-gray-50 shadow-lg p-10'>
+        <label className='text-lg font-bold block'>Email</label>
+        <input className='border block border-gray-500 rounded-lg px-4 py-2 w-96' type="text" />
+        <label  className='text-lg font-bold block'>Password</label>
+        <input className='border block border-gray-500 rounded-lg px-4 py-2 w-96' type="password" />
+        <button className='bg-sky-500 px-10 py-4 rounded-lg text-white font-semibold'>Login</button>
       </form>
     </div>
   );
