e8bcb04657b0466bf9f63d38b4ff0c3d8935008f
ritesh <riteshpatidar088@gmail.com>
2025-09-25 09:54:00 +0530

    udpated till the routing

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 function ProductDetails() {
-    const navigate = useNavigate()
-    const handleBackClick = () => {
-        navigate(-1)
-    }
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const handleBackClick = () => {
+    navigate(-1);
+  };
   return (
     <div>
-      Product details
-  
-    <button className='m-3 px-10 py-3 bg-emerald-500 text-white font-medium rounded-lg' onClick={handleBackClick}>Back</button>
+      Product details {id}
+      <button
+        className="m-3 px-10 py-3 bg-emerald-500 text-white font-medium rounded-lg"
+        onClick={handleBackClick}
+      >
+        Back
+      </button>
     </div>
-  )
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
