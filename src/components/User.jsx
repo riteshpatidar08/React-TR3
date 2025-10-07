38f45225b444d930657eb2498724b9dcfecf31a1
ritesh <riteshpatidar088@gmail.com>
2025-10-07 09:49:19 +0530

    updated till the context api

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { useContext } from 'react';
 import { DataContext } from '../context/DataContext';
 function User({ username }) {
-  const { username: newUsername, email } = useContext(DataContext);
-  console.log(newUsername);
-  console.log('user component is running');
+const {name , setname } = useContext(DataContext)
+console.log(name)
   return (
     <div className="border p-6 border-blue-500 m-3 rounded-sm">
       <h1>User component</h1>
       <h1>Show user name : {username}</h1>
-      <h1>{newUsername}</h1>
-      <h1>{email}</h1>
+     <h1>{name}</h1>
     </div>
   );
 }
 
 export default User;
+
+//create a context file and create a function to fetch data from jsonplaceholder api then shared this function via context api and call in a component to fetch the data .
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,18 +1,29 @@
 //NOTE STEP1 IMPORT CREATECONTEXT FN FROM REACT ;
-import { createContext, useState } from 'react';
+import { createContext  , useState} from "react";
 
 //NOTE STEP2 call the createContext fn and save its result with in a variable name same as user context file and export it
-export const DataContext = createContext();
+ export const DataContext = createContext()
 
 //NOTE STEP3 Create a PROVIDER COMPONENT where you can define the data which you are going to share across your react application.
-export const DataProvider = ({ children }) => {
-  const [username, setUsername] = useState('Data shared using context api');
-  console.log(children);
-  const [email, setEmail] = useState('test@gmail.com');
+export const DataProvider = ({children}) => {
+  const [name , setName] = useState('SHARED VIA CONTEXT')
+  // const sum  = (a,b) => {
+  //   return a + b
+  // }
   return (
-    <DataContext.Provider value={{ username, email }}>
-      {children}
+  <DataContext.Provider value={{name,setName,}}>
+{children} 
+{/* //children is equal to app */}
+ </DataContext.Provider>
+  )
+}
+   
       {/* here the value of children itself is app component = app */}
-    </DataContext.Provider>
-  );
-};
+ 
+
+
+
+//NOTE PROPS DRILLING 
+//NOTE CONTEXT FILE 
+
+
