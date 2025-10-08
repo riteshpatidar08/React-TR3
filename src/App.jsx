14b803b173cfba07afc99779f0a88f9684a98c09
ritesh <riteshpatidar088@gmail.com>
2025-10-08 10:55:56 +0530

    udpated till the redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import { useSelector  , useDispatch} from 'react-redux'
-import { increment } from './redux/slices/CounterSlice'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment } from './redux/slices/CounterSlice';
 function App() {
-const {count} = useSelector((state)=>state.count)
-const dispatch = useDispatch()
+  
+  const { count } = useSelector((state) => state.count);
+  const dispatch = useDispatch();
 
-const handleIncrement = () => {
-  dispatch(increment())
-}
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
   return (
-    <div className='font-bold text-3xl flex h-screen items-center justify-center flex-col'>
+    <div className="font-bold text-3xl flex h-screen items-center justify-center flex-col">
       <h1>{count}</h1>
-       {/* //replace this with count state variable */}
-       <button  onClick={handleIncrement} className='px-10 py-4 bg-blue-300 text-white font-bold'>Increment</button>
+      {/* //replace this with count state variable */}
+      <button
+        onClick={handleIncrement}
+        className="px-10 py-4 bg-blue-300 text-white font-bold"
+      >
+        Increment
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/redux/slices/CounterSlice.js
+++ src/redux/slices/CounterSlice.js
@@ -4,9 +4,7 @@ const counterSlice = createSlice({
   name: 'counter',
   initialState: { count: 0},
   reducers: {
-    increment: (state, action) => {
-      return { count: state.count + 1 };
-    },
+    increment: (state, action) => {state.count += 1}, //note in the toolkit we can direclty update the state variables because internally it uses immer library https://immerjs.github.io/immer/
     decrement: (state, action) => {
       return { count: state.count - 1 };
     },
