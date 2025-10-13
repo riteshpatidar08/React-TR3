956cb208eda2783bff478d00f03870197b31a31a
ritesh <riteshpatidar088@gmail.com>
2025-10-13 11:58:50 +0530

    updated till mdidlware

--- node/app.js
+++ node/app.js
@@ -1,14 +1,27 @@
-const express = require('express') ;
-const app = express() ;
+const express = require('express');
+const app = express();
 
-const PORT = 8000 ;
+const PORT = 8000;
 
-//NOTE MIDDLEWARES 
-// middleware are the functions in the express which have access to the req,res and next middleware function  , middleware can run any code and  access request and response object and modify them, then this modification on the req  can be available for next middleware in the req-res cylce.
-
-
-
-
-app.listen(PORT , ()=>{
-    console.log(`server is running on ${PORT}`)
+//NOTE MIDDLEWARES
+//NOTE => Middleware Are The Functions In The express which have access to the req,res and next middleware function  , middleware can run any code and  access request and response object and modify them, then this modification on the req  can be available for next middleware in the req-res cylce.
+app.use((req, res, next) => {
+  console.log('middleware first is running');
+  next()
+});
+app.use((req,res,next)=>{
+    console.log('middleware 2 is running')
+    next()
 })
+
+app.get('/health', (req, res) => {
+  res.send('running');
+});
+
+app.post('/name', (req,res)=>{
+    console.log(req.body)
+})
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
