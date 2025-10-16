2730b26377245f49a2409446fdd70329d5b6d9c1
ritesh <riteshpatidar088@gmail.com>
2025-10-16 15:20:37 +0530

    added user mdoel

--- node/app.js
+++ node/app.js
@@ -1,23 +1,25 @@
-const express = require('express') ;
-const mongoose = require('mongoose')
-const app = express() ;
-const userRoutes = require('./routes/userRoutes.js')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const userRoutes = require('./routes/userRoutes.js');
 app.use(express.json());
-const connectDB = async()=>{
-    try {
-        const connection = await mongoose.connect('mongodb://127.0.0.1:27017/mongodbTR3')
-        console.log('CONNECTION SUCCESSFULL')
-    } catch (error) {
-        console.log(error)
-    }
-}
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(
+      'mongodb://127.0.0.1:27017/mongodbTR3'
+    );
+    console.log('CONNECTION SUCCESSFULL');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-connectDB()
+connectDB();
 
-app.use('/api/v1' , userRoutes) ;
-
-app.listen(3000,()=>{
-    console.log('server is running on 3000')
-})
+app.use('/api/v1', userRoutes);
+app.use('/api/v1/auth', require('./routes/authRoutes.js'));
+app.listen(3000, () => {
+  console.log('server is running on 3000');
+});
 
 //crud apis
---
+++ node/controllers/authController.js
@@ -0,0 +1,22 @@
+
+exports.register = async (req, res) => {
+    try {
+      const { name, email, age } = req.body;
+      const userExist = await User.findOne({ email });
+      if (userExist) {
+        res.status(400).json({
+          message: 'user already exist',
+        });
+      }
+      const user = await User.create({ name, email, age });
+      if (!user) {
+        res.status(404).json({
+          message: 'failed',
+        });
+      }
+      res.status(201).json({
+        message: 'New user created',
+        user,
+      });
+    } catch (error) {}
+  };
